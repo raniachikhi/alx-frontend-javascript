371743ad5bb31688f77e3047c39e2ad1236a306c
raniachikhi <raniach0653@gmail.com>
2024-03-23 06:34:50 -0700

    add

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,8 +1,14 @@
-import { uploadPhoto, createUser } from '../alx-frontend-javascript/utils.js'
+import { uploadPhoto, createUser } from './utils.js';
 
 export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()])
-    .then((values) => {
-      console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
-    }).catch(() => console.log('Signup system offline'));
+	return Promise.all([uploadPhoto(), createUser()])
+	.then((result) => { 
+		console.log(
+        `${result[0].body} ${result[1].firstName} ${result[1].lastName}`,
+		);
+	})
+
+    	.catch((error) => {
+		console.log('Signup system offline');
+	});
 }
