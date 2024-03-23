d74ac86d97e3fda18ee6528c2d93fd4694365220
raniachikhi <raniach0653@gmail.com>
2024-03-23 06:31:04 -0700

    add

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,12 +1,12 @@
-export default function getFullResponseFromAPI(success) {
+function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
-    if (success === true) {
-      resolve({
-        status: 200,
-        body: "Success",
-      });
+    if (success) {
+      resolve({ status: 200, body: 'Success' });
     } else {
-      reject(new Error("The fake API is not working currently"));
+      reject(new Error('The fake API is not working currently'));
     }
   });
 }
+
+export default getFullResponseFromAPI;
+
